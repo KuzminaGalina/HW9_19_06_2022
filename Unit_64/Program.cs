92bc07dd8d48fde48c0ac89ca578fad5d2a49bae
Galina <kuzmina0497@mail.ru>
2022-06-19 20:10:46 +0300

    Добавлен алгоритм

---
+++ Unit_64/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 5; N = 1. -> "5, 4, 3, 2, 1"
+M = 8; N = 4. -> "8, 7,6, 5, 4"
+*/
+
+string NaturSpektr(int M, int N)
+{
+
+if (M == N)
+{
+    return $"{N} ";
+}
+else
+{
+  return $"{M}, " + NaturSpektr (M-1, N);
+}
+}
+
+Console.WriteLine(NaturSpektr(8, 4));
